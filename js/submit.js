ca4271f122096e33d01655c894a48cc8565970c0
Jonathan D. Forrider <jd4rider@gmail.com>
2018-04-23 02:31:23 -0600

    Login added

--- js/submit.js
+++ js/submit.js
@@ -13,7 +13,7 @@ if(window.localStorage.username == null){
     try{
         var username = url.searchParams.get("username"); 
     } catch(e){
-        var username;
+        var username = parse_query_string(location.search);
     }
         
     if(username == null){
@@ -76,6 +76,26 @@ function yay(){
                   window.location.href = '/read.html?id='+data._id.$oid;
                 }
         });
+}
+
+function parse_query_string(query) {
+  var vars = query.split("&");
+  var query_string = {};
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+      // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+      query_string[pair[0]] = arr;
+      // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
 }
 
 
